22063a5de44045d8def96f3ee9e160e3802eed3d
KomotsKE <alexkomov2005@gmail.com>
2025-11-11 11:52:12 +0500

    change alembic directory

--- app/infrastructure/database/alembic/env.py
+++ app/infrastructure/database/alembic/env.py
@@ -4,7 +4,7 @@ from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-from app.model.base import Base
+from app.infrastructure.database.base import Base
 from app.core.config import settings
 from alembic import context
 
