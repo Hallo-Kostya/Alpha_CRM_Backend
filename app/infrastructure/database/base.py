726bfb600575e0811ce42b82bf0b5d6bbbb0927b
KomotsKE <alexkomov2005@gmail.com>
2025-11-11 11:43:46 +0500

    Add structure for project

--- app/infrastructure/database/base.py
+++ app/infrastructure/database/base.py
@@ -1,23 +1,24 @@
-# from sqlalchemy import MetaData, DateTime, func
-# from sqlalchemy.orm import Mapped, mapped_column
-# from sqlalchemy.orm import DeclarativeBase
-# from app.core.config import settings
-# import uuid
-# from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import MetaData, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase
+from app.core.config import settings
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
 
+class Base(DeclarativeBase):
+    __abstract__ = True
 
-# class Base(DeclarativeBase):
-#     __abstract__ = True
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4
+        )
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True), default=func.now()
+    )
+    updated_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True), default=func.now(), onupdate=func.now()
+    )
 
-#     metadata = MetaData(naming_convention=settings.db.naming_convention)
-#     id: Mapped[uuid.UUID] = mapped_column(
-#         UUID(as_uuid=True),
-#         primary_key=True,
-#         default=uuid.uuid4
-#         )
-#     created_at: Mapped[DateTime] = mapped_column(
-#         DateTime(timezone=True), default=func.now()
-#     )
-#     updated_at: Mapped[DateTime] = mapped_column(
-#         DateTime(timezone=True), default=func.now(), onupdate=func.now()
-#     )
+#Реализация взаимодействия с бд
